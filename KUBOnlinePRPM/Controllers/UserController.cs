7a0c53bdbb4f3f61394f76f37cfc78b777f47c11
Hassan Abu Bakar <odin88@gmail.com>
2018-10-26 01:30:13 +0800

    add MyProfile page

--- KUBOnlinePRPM/Controllers/UserController.cs
+++ KUBOnlinePRPM/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KUBOnlinePRPM.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,25 @@ using System.Web.Mvc;
 
 namespace KUBOnlinePRPM.Controllers
 {
+
     public class UserController : Controller
     {
+        private KUBOnlinePREntities db = new KUBOnlinePREntities();
+
         // GET: User
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult MyProfile()
+        {
+            var userId = Int32.Parse(Session["UserId"].ToString());
+            var userDetail = db.Users
+                .Where(x => x.userId == userId)
+                .First();
+
+            return View(userDetail);
         }
     }
 }
